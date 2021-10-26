dc1edd9356c8c3e7136aeb509b8e5c585c2654e2
Othniel Timothy de Mesa Luzon <otluzon@student.uia.no>
2021-10-26 15:44:09 +0200

    added preprocessing section

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import numpy as np
 import os
 import cv2 as cv
 import pandas as pd
-
+from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 
 import PIL
@@ -27,9 +27,9 @@ train_images = []
 train_labels = []
 shape = (500, 500)
 
-for artists in os.listdir('Artists'):
+for artists in tqdm(os.listdir('Artists')):
     print(artists)
-    for filename in os.listdir('Artists/' + artists + '/train'):
+    for filename in tqdm(os.listdir('Artists/' + artists + '/train')):
         # Splitting images and storing image label into list
         img = cv.imread(os.path.join('Artists/' + artists + '/train/', filename))
 
@@ -46,4 +46,22 @@ train_images = np.array(train_images)
 x_train, x_val, y_train, y_val = train_test_split(train_images, train_labels, random_state=1)
 
 print('Execution complete')
+
+test_images = []
+test_labels = []
+for artists in tqdm(os.listdir('Artists')):
+    print(f'Test Artist: {artists}')
+    for filename in tqdm(os.listdir('Artists/' + artists + '/test')):
+        img = cv.imread(os.path.join('Artists/' + artists + '/test/', filename))
+
+        test_labels.append(artists.replace('_', ' '))
+
+        img = cv.resize(img, shape)
+
+        test_images.append(img)
+
+test_images = np.array(test_images)
+
+print('Execution complete: Test images added')
+
 
